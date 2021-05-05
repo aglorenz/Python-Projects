d66e87141def1b2e8daf26e9f3171d344999d059
Andrew Lorenz <aglorenz777@gmail.com>
2021-05-05 09:00:43 -0700

    Tkinter exercise

--- Python Exercises/tkintr1.py
+++ Python Exercises/tkintr1.py
@@ -16,18 +16,55 @@ class ParentWindow(Frame):
         # Creating a Tkinter variable
         self.varFName = StringVar()
         self.varLName = StringVar()
-        self.varFName.set('Bob')
-        self.varLName.set('Smith')
+##        self.varFName.set('Bob')
+##        self.varLName.set('Smith')
 
         # debug print to console
         print(self.varFName.get())
         print(self.varLName.get())
 
-        self.txtFName = Entry(self.master,text=self.varFName, font=("Helvetica", 16), fg='black', bg='lightblue')
-        self.txtFName.pack()
+        # First Name Label style and position
+        self.lblFName = Label(self.master,text = 'First Name: ', font=('Helvetica', 16), fg='black', bg='lightgrey' )
+        self.lblFName.grid(row=0, column=0, padx=(30,0), pady=(30,0))
 
+        # Last Name Label style and position
+        self.lblLName = Label(self.master,text = 'Last Name: ', font=('Helvetica', 16), fg='black', bg='lightgrey' )
+        self.lblLName.grid(row=1, column=0, padx=(30,0), pady=(30,0))
+        
+        # Empty label with style and position to put our text after clicking Submit button
+        self.lblDisplay = Label(self.master,text = '', font=('Helvetica', 16), fg='black', bg='lightgrey' )
+        self.lblDisplay.grid(row=3, column=1, padx=(30,0), pady=(30,0))
+        
+        #for details go to TKinter Pack Geometry Manager
+        #self.txtFName.pack()
+        # TKinter grid manager at effbot.org
+        
+        # Text box to the right of First Name label 
+        self.txtFName = Entry(self.master,text=self.varFName, font=("Helvetica", 16), fg='black', bg='lightblue')
+        self.txtFName.grid(row=0, column=1, padx=(30,0), pady=(30,0))
+
+        # Text box to the right of the Last Name label
         self.txtLName = Entry(self.master,text=self.varLName, font=("Helvetica", 16), fg='black', bg='lightblue')
-        self.txtLName.pack()
+        self.txtLName.grid(row=1, column=1, padx=(30,0), pady=(30,0))
+
+        # Submit button to call the submit method
+        self.btnSubmit = Button(self.master, text = "Submit", width=10, height=2, command=self.submit)
+        self.btnSubmit.grid(row=2, column=1, padx=(0,0), pady=(30,0), sticky=NE)
+
+        # Cancel button to close the window
+        self.btnCancel = Button(self.master, text = "Cancel", width=10, height=2, command=self.cancel)
+        self.btnCancel.grid(row=2, column=1, padx=(0,90), pady=(30,0), sticky=NE)
+
+    # Method to get the first and last name and print it in the empty (originally) label box (it's not a text box)
+    def submit(self):
+        fn = self.varFName.get()
+        ln = self.varLName.get()
+        self.lblDisplay.config(text='Hello {} {}!'.format(fn,ln))
+
+    # Method to close the window
+    def cancel(self):
+        self.master.destroy()
+
 
 
 if __name__ == "__main__":
