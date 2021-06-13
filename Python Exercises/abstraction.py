14bcf368f67df2e760dee658518ba4d3d4373e18
Andrew Lorenz <aglorenz777@gmail.com>
2021-06-12 17:23:01 -0700

    Update comments abstraction.py

--- Python Exercises/abstraction.py
+++ Python Exercises/abstraction.py
@@ -1,23 +1,24 @@
 from abc import ABC, abstractmethod
 
-class Car(ABC):
+class Car(ABC): # ABC = ABstract Class, imported from abc
     def pay_slip(self, amount):
         print("Your purchase amount: ", amount)
 
-    # this function is telling us to pass in an argument, but we won't tell
-    # you how or what kind of data it will be.
-    @abstractmethod
+    # This is an abstract method.  It is telling us to pass in an argument, but we won't tell
+    # you what kind of data it will be.
+    @abstractmethod  # imported from abc
     def payment(self, amount):
         pass
 
+# Child Class must define the implementation of its parent's abstract method
 class DebitCardPayment(Car):
-    # here we implement the payment function from it's parent car class
+    # Here we implement the payment method from the parent Car class
     def payment(self, amount):
         print("Your purchase amount of {} exceeded your $100 limit ".format(amount))
 
 obj = DebitCardPayment()
 obj.pay_slip("$400")
-obj.payment("$400")
+obj.paysment("$400")
              
 # Output
 # Your purchase amount:  $400
