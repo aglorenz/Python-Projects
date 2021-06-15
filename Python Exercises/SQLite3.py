f89b4c46e3db7cd3435fa8e37e2974f51b76a8a8
Andrew Lorenz <aglorenz777@gmail.com>
2021-06-15 14:59:03 -0700

    latest changes

--- Python Exercises/SQLite3.py
+++ Python Exercises/SQLite3.py
@@ -5,7 +5,7 @@ first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 age = int(input("Enter your age: "))
 
-# execute insewrt statement for supplied person data
+# execute insert statement for supplied person data
 with sqlite3.connect('test_database.db') as connection:
     c = connection.cursor()
     line = "INSERT INTO People VALUES ('"+ first_name +"', '"+ last_name +"', " +str(age) +")"
---
+++ Python Exercises/tkinter_practice.py
@@ -0,0 +1,39 @@
+from tkinter import *
+win = Tk()
+f = Frame(win)
+b1 =  Button(f, text="One")
+b2 =  Button(f, text="Two")
+b3 =  Button(f, text="Three")
+b1.pack(side=LEFT)
+b2.pack(side=LEFT)
+b3.pack(side=LEFT)
+l = Label(win, text = "This label  is over all buttons")
+l.pack()
+f.pack()
+
+def but1():
+   print(v.get())
+
+def but2():
+    v.set("This is set by the program when clicking button 2")
+
+b1.configure(command=but1)
+b2.configure(command=but2)
+sb = Scrollbar(win, orient=VERTICAL)
+sb.pack(side=LEFT, fill=Y)
+
+v=StringVar()
+e=Entry(win, textvariable = v)
+e.pack()
+
+lb=Listbox(win,height=3)
+lb.pack()
+lb.insert(END, "first entry")
+lb.insert(END, "second entry")
+lb.insert(END, "third entry")
+lb.insert(END, "fourth entry")
+
+# Tie the scroll bar to the listbox
+sb.configure(command=lb.yview)
+lb.configure(yscrollcommand=sb.set)
+lb.curselection() # will return a tuple of the items selected
