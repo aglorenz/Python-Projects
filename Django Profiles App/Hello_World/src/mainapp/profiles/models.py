fa49381671ff1d26171e45d8791ecc218e0ed2d4
Andrew Lorenz <aglorenz777@gmail.com>
2022-07-03 17:34:22 -0700

    showing first and last name

--- Django Profiles App/Hello_World/src/mainapp/profiles/models.py
+++ Django Profiles App/Hello_World/src/mainapp/profiles/models.py
@@ -21,7 +21,7 @@ class Profile(models.Model):
     objects = models.Manager()
 
     def __str__(self):
-        return self.first_name
+        return self.first_name + ' ' + self.last_name
 
 # two entries get inserted into the table when you run python manage.py runserver
 # but if you run python manage.py runserver --noreload you only get 1 row added
