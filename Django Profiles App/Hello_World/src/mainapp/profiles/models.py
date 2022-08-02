17695207f61370e2a96ccf6a1f6192da9e07c233
Andrew Lorenz <aglorenz777@gmail.com>
2022-08-02 16:38:39 -0700

    working on styling and cache busting

--- Django Profiles App/Hello_World/src/mainapp/profiles/models.py
+++ Django Profiles App/Hello_World/src/mainapp/profiles/models.py
@@ -21,7 +21,8 @@ class Profile(models.Model):
     objects = models.Manager()
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        name = '{} {}'.format(self.first_name, self.last_name)
+        return name
 
 # two entries get inserted into the table when you run python manage.py runserver
 # but if you run python manage.py runserver --noreload you only get 1 row added
